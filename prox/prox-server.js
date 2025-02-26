a378d97e106fb3b5051be86465dc10baeb8b81e3
TIXU-RAMP <niggxs.with.frys@gmail.com>
2025-02-26 00:57:39 -0500

    Update prox-server.js

--- prox/prox-server.js
+++ prox/prox-server.js
@@ -1,1 +1,11 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
 
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer((req, res) => {
+  proxy.web(req, res, { target: 'http://example.com' });
+});
+
+server.listen(8000, () => {
+  console.log('Proxy server is running on port 8000');
+});
