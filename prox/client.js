b6eeea777788aeacfb72ed2111ece9c6a82f7ccb
TIXU-RAMP <niggxs.with.frys@gmail.com>
2025-02-28 06:42:09 -0500

    Delete prox directory

--- prox/client.js
+++
@@ -1,4 +0,0 @@
-function proxy() {
-  const url = document.getElementById('url').value;
-  window.location.href = `/proxy?url=${encodeURIComponent(url)}`;
-}
--- prox/logger.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-function logRequest(req) {
-  const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-  fs.appendFileSync('requests.log', log);
-}
-
-module.exports = { logRequest };
